b9d023e875aa2e1e7025b8e7a6c0c954fb9fbd88
Muhammad Habib Rohman <mhrohman@live.com>
2017-06-08 15:50:44 +0700

    git(fix): untracked git

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Check wether bisskey code is valid bisskey code or not
- *
- * @param bisskey {string} bisskey code
- */
-export declare const check: (bisskey: string) => boolean;
-export default check;
